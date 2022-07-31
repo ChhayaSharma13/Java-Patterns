97ced16ded54af80871992927529f8214090eb8b
ChhayaSharma <chhayasharma1304@gmail.com>
2022-07-31 21:55:50 +0100

    Mirrored - DownRightTriangle Star Pattern - with spaces

--- src/MirroredDownRightTriangle.java
+++ src/MirroredDownRightTriangle.java
@@ -7,12 +7,22 @@ public class MirroredDownRightTriangle {
         int n = sc.nextInt();
 
         int i,j,k;
-        for (i=1; i<=n;i++){
+        /*for (i=1; i<=n;i++){
             for (j=1; j<=i-1;j++){
                 System.out.print(" ");
             }
             for (k=(n-i)+1;k>=1;k--){
                 System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+        for (i=1; i <=n;i++){
+            for (j=1; j <= 2*(i-1);j++){
+                System.out.print(" ");
+            }
+            for (k = 2*n-i; k >=n;k--){
+                System.out.print("* ");
             }
             System.out.println();
         }
