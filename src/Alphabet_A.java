7d66f3c89e9ced89540bfb29e805199b55dd818a
ChhayaSharma <chhayasharma1304@gmail.com>
2022-08-02 18:16:07 +0100

    Alphabet A - Star Pattern

---
+++ src/Alphabet_A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Alphabet_A {
+    public static void main(String[] args) {
+        System.out.println("Enter n:");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        int i, j;
+        for(i=1; i<=n;i++){
+            for (j=1;j<=2*n;j++) {
+                if (j == (n - i + 1) || j == (n + i)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
